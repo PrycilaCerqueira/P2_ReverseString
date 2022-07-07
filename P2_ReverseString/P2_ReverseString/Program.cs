d19aa4a2a1a30a0dda23d8aad6d58072e7077886
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-07-06 21:49:10 -0400

    Accessing an item into the string.

--- P2_ReverseString/P2_ReverseString/Program.cs
+++ P2_ReverseString/P2_ReverseString/Program.cs
@@ -6,7 +6,13 @@ namespace P2_ReverseString // Note: actual namespace depends on the project name
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String word;
+            String reverseWord;
+            
+            Console.WriteLine("Let's play! Enter a word and we will reverse it for you. ");
+            word = Console.ReadLine();
+
+            Console.WriteLine($"You wrote {word[1]}");
         }
     }
 }
