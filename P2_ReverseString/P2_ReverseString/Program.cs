1b9c557a27f84fd4fc41713d8c3a324d7f1421ba
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-07-06 22:28:17 -0400

    Final version of the FOR Loop

--- P2_ReverseString/P2_ReverseString/Program.cs
+++ P2_ReverseString/P2_ReverseString/Program.cs
@@ -11,11 +11,10 @@ namespace P2_ReverseString // Note: actual namespace depends on the project name
 
             Console.WriteLine("Let's play! Enter a word and we will reverse it for you. ");
             word = Console.ReadLine();
-            reverseWord = word;
-
+           
             for (int count = word.Length -1; count >= 0; count--)
             {
-                reverseWord = reverseWord + reverseWord[count];
+                reverseWord = reverseWord + word[count];
             }
 
             Console.WriteLine($"You wrote {reverseWord}");
